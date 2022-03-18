922ba763f234cab1a6dbb316713de044350a4054
Marko Stojiljkovic <stojiljkovicmarko@yahoo.com>
2022-03-18 12:37:25 +0100

    Updates

--- src/model/drag-drop.ts
+++
@@ -1,9 +0,0 @@
-export interface Draggable {
-    dragStartHandler(event: DragEvent): void;
-}
-
-export interface DragTarget {
-    dragOverHandler(event: DragEvent): void;
-    dropHandler(event: DragEvent): void;
-    dragLeaveHandler(event: DragEvent): void;
-}
